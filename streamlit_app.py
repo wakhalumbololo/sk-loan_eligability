b274c9ce2030d07bdd4ba3945181fefa85d1265e
wakhalumbololo <skchelsea3105@gmail.com>
2025-03-03 23:25:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import sklearn.ensamble import RandomForestClassifier
+import sklearn.ensemble import RandomForestClassifier
 
 
 #title and description
