d57938860ccd4be6af455dfb7d61ef943f69f055
wakhalumbololo <skchelsea3105@gmail.com>
2025-03-03 19:44:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,13 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+#title and description
 st.title('😁 Loan Eligability System')
 
 st.write('this is a web application for a machine learning model for a loan eligability system')
 
+#expanders to display dataset info
+with st.expander("Data"):
+  st.write("**Here's the raw data")
+  df = pd.read_csv(r"C:\Users\skche\OneDrive\Desktop\project 4 planning\loan eligability ds\train.csv")
+  df
